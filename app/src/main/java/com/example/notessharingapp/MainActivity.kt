faee2bcf724c16a04c8a2b0065ad10190abcf2a6
piyush <22341@iiitu.ac.in>
2023-09-27 02:01:37 +0530

    Login Push

--- app/src/main/java/com/example/notessharingapp/MainActivity.kt
+++ app/src/main/java/com/example/notessharingapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.notessharingapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.notessharingapp.databinding.ActivityMainBinding
+import com.example.notessharingapp.databinding.SignupActivityBinding
+import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout
+import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
     }
 }
